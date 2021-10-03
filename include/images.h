e0a26a558f4fd596df5c7b6770b0697ed9ae887a
Rene Zeldenthuis <rzeldent@outlook.com>
2021-10-04 01:29:17 +0200

    Updated images Updated layout Updated README.md

---
+++ include/images.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <timage.h>
+
+#define IMAGE_TRANSPARTENT_COLOR	0x0000
+
+const extern tImage image_splash;
+const extern tImage image_radiation_32x32;
+const extern tImage image_radiation_64x64;
